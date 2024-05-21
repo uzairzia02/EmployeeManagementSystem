c9d8e31e75bc98ccb77fb7f358f51e878b986248
uzairzia <uzair.governorsindh@gmail.com>
2024-05-21 23:13:09 +0500

    first commit

---
+++ sms.ts
@@ -0,0 +1,106 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+
+interface Employees {
+  empID: number;
+  empName: string;
+  empSalary: number;
+  empAge: number;
+  empDepartment: string;
+  joiningDate: Date;
+}
+
+console.log("------------------------------------------------------------------\n            WELCOME TO EMPLOYEE MANAGEMENT SYSTEM\n------------------------------------------------------------------");
+
+
+let AllEmployeesData: Employees[] = [];
+
+async function NewEmployee() {
+  let empID = await inquirer.prompt({
+    name: "empID",
+    type: "number",
+    message: "Please Enter the Employee ID: ",
+  });
+  let empName = await inquirer.prompt({
+    name: "empName",
+    type: "input",
+    message: "Please Enter Employee Name: ",
+  });
+  let empSalary = await inquirer.prompt({
+    name: "empSalary",
+    type: "number",
+    message: "Please Enter Employee Salary: ",
+  });
+  let empAge = await inquirer.prompt({
+    name: "empAge",
+    type: "number",
+    message: "Please Enter Employee Age: ",
+  });
+  let empDepartment = await inquirer.prompt({
+    name: "empDepartment",
+    type: "input",
+    message: "Please Enter Employee Department: ",
+  });
+  let joiningDate = await inquirer.prompt({
+    name: "joiningDate",
+    type: "input",
+    message: "Please Enter Employee Joining Date: ",
+  });
+  let additionOfEmployee: Employees = {
+    empID: empID.empID,
+    empName: empName.empName,
+    empSalary: empSalary.empSalary,
+    empAge: empAge.empAge,
+    empDepartment: empDepartment.empDepartment,
+    joiningDate: joiningDate.joiningDate,
+  };
+  AllEmployeesData.push(additionOfEmployee);
+  console.table(AllEmployeesData);
+
+  let addMore = await inquirer.prompt({
+    name: "addMore",
+    type: "confirm",
+    message: "Do you want to add more employee?",
+  });
+  if (addMore.addMore) {
+    NewEmployee();
+  } else {
+    let nextAction = await inquirer.prompt({
+      name: "nextAction",
+      type: "list",
+      message: "What do you want to do next?",
+      choices: ["Delete Employee", "Exit"],
+    });
+    if (nextAction.nextAction == "Exit") {
+      console.table(AllEmployeesData);
+      process.exit();
+    } else if (nextAction.nextAction == "Delete Employee") {
+      let deleteEmployee = await inquirer.prompt({
+        name: "deleteEmployee",
+        type: "list",
+        message: "Choose an employee to delete",
+        choices: AllEmployeesData.map((a) => ({
+          name: a.empName,
+          value: a.empID,
+        })),
+      });
+      AllEmployeesData = AllEmployeesData.filter(
+        (employee) => employee.empID != deleteEmployee.deleteEmployee
+      );
+      console.table(AllEmployeesData);
+      let finalInput = await inquirer.prompt({
+        name: "finalInput",
+        type: "list",
+        message: "What do you want to do now?",
+        choices: ["Add new employee", "exit"],
+      });
+      if (finalInput.finalInput === "Add new employee") {
+        NewEmployee();
+      } else {
+        console.table(AllEmployeesData);
+        process.exit(0);
+      }
+    }
+  }
+}
+NewEmployee();
